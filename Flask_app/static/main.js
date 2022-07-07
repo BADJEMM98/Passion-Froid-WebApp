6c18c823a6d3e34a5d8113717561a950207b7dec
Your Name <tbowetumawe@myges.fr>
2022-07-07 21:58:46 +0200

    flask2

--- Flask_app/static/main.js
+++ Flask_app/static/main.js
@@ -1,36 +1,61 @@
-console.log("Done al")
-
 
 /*  ==========================================
     SHOW UPLOADED IMAGE
 * ========================================== */
 function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#imageResult')
-                .attr('src', e.target.result);
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
+    reader.onload = function(e) {
+      $('#imageResult')
+        .attr('src', e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
 }
 
-$(function () {
-    $('#upload').on('change', function () {
-        readURL(input);
-    });
+$(function() {
+  $('#app').on('change', function() {
+    readURL(input);
+  });
 });
 
 /*  ==========================================
     SHOW UPLOADED IMAGE NAME
 * ========================================== */
-var input = document.getElementById( 'upload' );
-var infoArea = document.getElementById( 'upload-label' );
+var input = document.getElementById('upload');
+var infoArea = document.getElementById('upload-label');
 
-input.addEventListener( 'change', showFileName );
-function showFileName( event ) {
+input.addEventListener('change', showFileName);
+
+function showFileName(event) {
   var input = event.srcElement;
   var fileName = input.files[0].name;
   infoArea.textContent = 'File name: ' + fileName;
 }
+
+
+
+// const v = new Vue({
+//   el: '#app',
+//   template: `
+//     <p>
+//       <input type="file" accept="image/*" @change="uploadImage">
+//       <img :src="previewImage" />
+//     </p>
+//   `,
+//   data: {
+//     previewImage: undefined
+//   },
+//   methods: {
+//     uploadImage(e) {
+//       const [image] = e.target.files;
+//       const reader = new FileReader();
+//       reader.readAsDataURL(image);
+//       reader.onload = e => {
+//         this.previewImage = e.target.result;
+//         console.log(this.previewImage);
+//       };
+//     }
+//   }
+// })
