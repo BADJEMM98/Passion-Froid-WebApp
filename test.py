bf4e32b8b43d0aef5c9da54a5492499d4acb5db6
badje <badjemm@3il.fr>
2022-07-07 21:38:22 +0200

    requete de recherche par tag
    Signed-off-by: badje <badjemm@3il.fr>

--- test.py
+++ test.py
@@ -7,16 +7,20 @@ SQL_SERVER = 'passion-mysql-server.mysql.database.azure.com'
 SQL_DB = 'passiondb'
 USERNAME = 'passionadmin'
 PASSWORD = 'MonikMik17!'
+list_tags = ('arbre','mangue')
 
 
+# recherche1 = "SELECT id_tag FROM tag_image INNER JOIN tags ON id = id_tag WHERE name =" + str(tag)+";"
+# recherche = "SELECT * FROM images as im INNER JOIN tag_image as ti ON im.id = ti.id_image INNER \
+#             JOIN tags t ON t.id = ti.id_tag WHERE t.id IN " + str(list_tags)+ " GROUP BY im.id,im.name HAVING COUNT(*)="+str(len(list_tags)) + ";"
+# recherche = "SELECT A.id_image FROM tag_image A INNER JOIN (SELECT id FROM tags \
+#             WHERE name IN " + str(list_tags) + "GROUP BY id HAVING COUNT(*) = "+ str(len(list_tags)) + ") AS B ON A.id_tag = B.id"
+
+recherche = "SELECT images.bloblink from images INNER JOIN ( SELECT A.id_image FROM tag_image A INNER JOIN \
+            ( SELECT id FROM tags WHERE name IN"+ str(list_tags)+") AS B ON A.id_tag = B.id GROUP BY A.id_image \
+            HAVING COUNT(*) = "+ str(len(list_tags))+") AS C ON images.id = C.id_image;"
 try:
-#    cnxn = mysql.connector.connect(user=USERNAME, password=PASSWORD, host=SQL_SERVER, port=3306, database=SQL_DB, client_flags=[mysql.connector.ClientFlag.SSL],ssl_ca=SSL_CA, ssl_disabled=True)
-   cnxn = pymysql.connect(host=SQL_SERVER,
-                            port=3306,
-                             user=USERNAME,
-                             password=PASSWORD,                             
-                             database=SQL_DB
-                             )
+   cnxn = mysql.connector.connect(user=USERNAME, password=PASSWORD, host=SQL_SERVER, port=3306, database=SQL_DB)
    print("Connection established")
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -31,14 +35,30 @@ else:
     #Création de la table Images
     # cursor.execute("CREATE TABLE images (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50), bloblink VARCHAR(100) );")
     # cursor.execute("CREATE TABLE tags (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50) );")
-    # cursor.execute("CREATE TABLE tag_image (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(50) );")
-    cursor.execute("INSERT INTO images (name,bloblink) VALUES ('manguier.jpeg','www/tmp/blob1');")
-    print("Finished inserting row.")
+    # cursor.execute("CREATE TABLE tag_image (id_image INT NOT NULL, id_tag INT NOT NULL, PRIMARY KEY(id_image, id_tag));")
+    # cursor.execute("ALTER TABLE tag_image ADD CONSTRAINT FOREIGN KEY (id_image) REFERENCES images(id);")
+    # cursor.execute("ALTER TABLE tag_image ADD CONSTRAINT FOREIGN KEY (id_tag) REFERENCES tags(id);")
+    # cursor.execute("INSERT INTO images (name,bloblink) VALUES ('banane.jpeg','www/tmp/blob1');")
+    # cursor.execute("INSERT INTO images (name,bloblink) VALUES ('ananas.jpeg','www/tmp/blob1');")
+    # cursor.execute("INSERT IGNORE INTO tags (name) VALUES ('arbre');")
+    # cursor.execute("INSERT INTO tag_image (id_image,id_tag) VALUES (1,5);")
+    # cursor.execute("INSERT INTO tag_image (id_image,id_tag) VALUES (1,3);")
+    # cursor.execute("INSERT INTO tag_image (id_image,id_tag) VALUES (3,2);")
+    # cursor.execute("INSERT INTO tag_image (id_image,id_tag) VALUES (4,5);")
+    # cursor.execute("INSERT INTO tag_image (id_image,id_tag) VALUES (5,4);")
+    
 
-    cursor.execute("SELECT * FROM images;")
+    # print("Finished inserting row.")
+    # cursor.execute("SELECT LAST_INSERT_ID();")
+    # rows = cursor.fetchall()
+    # print(rows)
+    # cursor.execute("")
+    # cursor.execute("SELECT * FROM tags;")
+    # cursor.execute("SELECT DISTINCT id_image, id_tag FROM tag_image WHERE id_tag=5")
+    cursor.execute(recherche)
     rows = cursor.fetchall()
     for row in rows:
-        print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+        print("Data row = (%s, %s)" %(str(row[0]), str(row[1]))) #, str(row[2])))
 
     # print("Finished creating table.")
 
